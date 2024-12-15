344818a635c1f49ed7ac6c3102589dc36ac02320
Piya Samyak (IT RDI ADS) <samyakpiya@gmail.com>
2024-12-15 12:47:57 -0500

    style: update font styling and add custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <h1 className="text-red-500">App</h1>;
+  return <h1 className="text-5xl font-bold text-violet-300">App</h1>;
 };
 
 export default App;
